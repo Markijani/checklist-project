24e4714aca488562922890b8e1578587f2cd65c0
Марина <marina.morozevich@gmail.com>
2024-02-19 10:52:33 +0300

    feature: 19.02.2024 10:52 created Form and FormRepo

---
+++ checklist-project/src/main/java/ru/itgirl/checklistproject/model/entity/Form.java
@@ -0,0 +1,56 @@
+package ru.itgirl.checklistproject.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+@Table(name = "forms")
+@Data
+@NoArgsConstructor
+public class Form {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "group_num")
+    private int groupNum;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "level_1_answers")
+    private Map<String, Integer> level1Answers = new HashMap<>();
+
+    @Column(name = "level_2_answers")
+    private Map<String, Integer> level2Answers = new HashMap<>();
+
+    @Column(name = "level_3_answers")
+    private Map<String, Integer> level3Answers = new HashMap<>();
+
+    public Form (String userName, int groupNum) {
+        this.userName = userName;
+        this.groupNum = groupNum;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public void addLevel1Answer(String question, Integer answer) {
+        level1Answers.put(question, answer);
+    }
+
+    public void addLevel2Answer(String question, Integer answer) {
+        level2Answers.put(question, answer);
+    }
+
+    public void addLevel3Answer(String question, Integer answer) {
+        level3Answers.put(question, answer);
+    }
+}
