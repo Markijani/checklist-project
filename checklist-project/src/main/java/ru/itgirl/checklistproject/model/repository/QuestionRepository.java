b04ca0f240b701e4ec5d2542929cd8880393ad4d
Ульяна <skipina_ulyana24@mail.ru>
2024-02-20 18:13:41 +0500

    Migration, QuestionRepository

---
+++ checklist-project/src/main/java/ru/itgirl/checklistproject/model/repository/QuestionRepository.java
@@ -0,0 +1,8 @@
+package ru.itgirl.checklistproject.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<QuestionRepository, Long> {
+}
